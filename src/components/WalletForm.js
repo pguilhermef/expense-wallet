841757d293835326678f75394c130ab606fa5e5b
pguilhermef <pguilhermesantos@hotmail.com>
2022-08-26 17:52:00 -0300

    Início do projeto, vamo que vamo!

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <div>Commit inicial</div>
     );
   }
 }
