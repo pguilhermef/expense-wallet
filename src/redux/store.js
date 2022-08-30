e78f6615640762db45d81a6520f3a7e72d17cb33
pguilhermef <pguilhermesantos@hotmail.com>
2022-08-29 22:44:36 -0300

    [Feature] Adiciona o redux-thunk, adiciona todos os reducers dentro de root reducer e cria middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,10 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import rootReducer from './reducers/index';
 
-const Store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk),
-  ),
-);
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk),
+));
 
-export default Store;
+export default store;
