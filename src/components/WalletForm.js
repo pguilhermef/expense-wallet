277513a5fe016e3096d440ab96fa7141f2d7c4d5
pguilhermef <pguilhermesantos@hotmail.com>
2022-08-31 15:43:38 -0300

    feat: :sparkles: Rendeniza Walletform em Wallet

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,1 +1,16 @@
-/* Viajei, crie aqui o componente Wallet Form. */
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export class WalletForm extends Component {
+  render() {
+    return (
+      <div>WalletForm</div>
+    );
+  }
+}
+
+export default WalletForm;
+
+// WalletForm.propTypes = {
+
+// };
