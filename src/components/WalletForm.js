8d1e044b9f6b31569c258b72f8bdcdf99c78e2a6
pguilhermef <pguilhermesantos@hotmail.com>
2022-08-31 16:21:07 -0300

    feat: :sparkles: Adiciona campos de input e lógica de salvamento no estado

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -2,9 +2,99 @@
 import React, { Component } from 'react';
 
 export class WalletForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      paymentMethod: 'Dinheiro',
+      categoryTag: 'Alimentação',
+    };
+  }
+
+  handlePaymentMethod = ({ target }) => {
+    const { value } = target;
+    const paymentMethod = value;
+
+    this.setState({
+      paymentMethod,
+    });
+  };
+
+  handleCategoryTag = ({ target }) => {
+    const { value } = target;
+    const categoryTag = value;
+
+    this.setState({
+      categoryTag,
+    });
+  };
+
   render() {
     return (
-      <div>WalletForm</div>
+      <section>
+        <label htmlFor="value-input">
+          Valor:
+          <input
+            id="value-input"
+            type="number"
+            data-testid="value-input"
+          />
+        </label>
+
+        <select data-testid="currency-input">
+          <option>
+            OLHA O README REQ 3
+          </option>
+          <option>
+            BRL
+          </option>
+        </select>
+
+        <select
+          data-testid="method-input"
+          onChange={ this.handlePaymentMethod }
+        >
+          <option>
+            Dinheiro
+          </option>
+          <option>
+            Cartão de crédito
+          </option>
+          <option>
+            Cartão de débito
+          </option>
+        </select>
+
+        <label htmlFor="description-input">
+          Descrição
+          <input
+            id="description-input"
+            type="text"
+            data-testid="description-input"
+          />
+        </label>
+
+        <select
+          data-testid="tag-input"
+          onChange={ this.handleCategoryTag }
+        >
+          <option>
+            Alimentação
+          </option>
+          <option>
+            Lazer
+          </option>
+          <option>
+            Trabalho
+          </option>
+          <option>
+            Transporte
+          </option>
+          <option>
+            Saúde
+          </option>
+        </select>
+      </section>
     );
   }
 }
