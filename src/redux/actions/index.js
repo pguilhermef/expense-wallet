daa259150b5be48c0afa17788fde19ff285c57f8
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-03 09:44:54 -0300

    feat: :sparkles: Cria actions referentes à requisição da api de moedas

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,36 @@
+import getAllCurrencies from '../../services/Get_Currencies';
+
 export const GET_USER_EMAIL = 'GET_USER_EMAIL';
 
 export const getUserEmail = (email) => ({
   type: GET_USER_EMAIL,
   email,
 });
+
+export const GET_CURRENCIES = 'GET_CURRENCIES';
+export const GET_CURRENCY_SUCCESS = 'GET_CURRENCY_SUCCESS';
+export const GET_CURRENCY_FAILURE = 'GET_CURRENCY_FAILURE';
+
+export const getCurrencies = () => ({
+  type: GET_CURRENCIES,
+});
+
+export const getCurrenciesSucess = (currencies) => ({
+  type: GET_CURRENCY_SUCCESS,
+  currencies,
+});
+
+export const getCurrenciesFailure = (error) => ({
+  type: GET_CURRENCY_FAILURE,
+  error,
+});
+
+export const requestCurrenciesThunk = () => async (dispatch) => {
+  dispatch(getCurrencies());
+  try {
+    const allCurrencies = await getAllCurrencies();
+    dispatch(getCurrenciesSucess(allCurrencies));
+  } catch (error) {
+    dispatch(getCurrenciesFailure(error.message));
+  }
+};
