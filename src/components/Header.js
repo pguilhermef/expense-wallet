46da916f4628cdb0a8f18576a3557855c58a7f00
pguilhermef <pguilhermesantos@hotmail.com>
2022-09-05 19:59:39 -0300

    fix: :bug: altera a lógica de soma das expanses para dentro do render

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,28 +3,20 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
-  handleSumOfExpenses = (expenses) => {
-    const valuesConverted = expenses
-      .map((expense) => {
-        const currencyVariation = Object.values(expense.exchangeRates)
-          .find((currency) => currency.code === expense.currency).ask;
-
-        return expense.value * currencyVariation;
-      });
-
-    return valuesConverted
-      .reduce((acc, curr) => acc + curr, 0);
-  };
-
   render() {
     const { email, expenses } = this.props;
+
+    const sumOfExpenses = expenses
+      .map((expense) => expense.value * Object.values(expense.exchangeRates)
+        .find((currency) => currency.code === expense.currency).ask)
+      .reduce((acc, curr) => acc + curr, 0);
     return (
       <section>
         <div>
           <span>Olá, </span>
           <span data-testid="email-field">{ email }</span>
           <span> Despesa Total: </span>
-          <span data-testid="total-field">{ this.handleSumOfExpenses(expenses) }</span>
+          <span data-testid="total-field">{ parseFloat(sumOfExpenses).toFixed(2) }</span>
           <span data-testid="header-currency-field">BRL</span>
         </div>
       </section>
